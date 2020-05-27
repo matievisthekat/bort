79211748a5f93b63e033de749c761a8c1c219daf
MatievisThKat <matievisthekat@gmail.com>
2020-05-27 10:34:23 +0200

    add: manager query to mutual guilds endpoint

--- src/structures/base/Client.js
+++ src/structures/base/Client.js
@@ -95,7 +95,7 @@ module.exports = class Bort extends Client {
         let guilds = [];
         for (const guild of this.guilds.cache.array()) {
           const member = await guild.members.fetch(userID).catch(() => {});
-          if (req.query.manager)
+          if (req.query.manager && member.hasp)
           if (member) guilds.push(guild);
         }
 
