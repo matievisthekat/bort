0ab8df7b446020ddf1888c6e53ffa13dc118a865
MatievisThKat <matievisthekat@gmail.com>
2020-06-12 22:49:57 +0200

    create minesweeper command

---
+++ src/commands/Core/minesweeper.js
@@ -0,0 +1,30 @@
+const Command = require("../../structures/base/Command");
+const Minesweeper = require("discord.js-minesweeper");
+
+module.exports = class Name extends Command {
+  constructor() {
+    super({
+      name: "minesweeper",
+      aliases: ["ms"],
+      category: "Core",
+      description: "Play a game of minesweeper",
+      requiresArgs: false,
+      guildOnly: false
+    });
+  }
+
+  async run(msg, args, flags) {
+    const minesweeper = new Minesweeper({
+      rows: 9,
+      columns: 9,
+      mines: 10,
+      emote: "boom",
+      revealFirstCell: false,
+      returnType: "emoji"
+    });
+    const matrix = minesweeper.start();
+
+    const embed = new msg.client.embed().blue.setDescription(matrix);
+    msg.channel.send(embed);
+  }
+};
