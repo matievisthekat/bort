3d1bf79b025e884c0868eab62fce4b3e1a079e25
MatievisTheKat <matievisthekat@gmail.com>
2020-07-02 15:47:37 +0200

    add all flag to sell command

--- src/commands/Currency/sell.js
+++ src/commands/Currency/sell.js
@@ -7,7 +7,8 @@ module.exports = class Sell extends Command {
       category: "Currency",
       description: "Sell some of your hard earned items",
       usage: "{item ID}",
-      examples: ["dog", "ultrachicken"],
+      flags: ["all"],
+      examples: ["dog", "ultrachicken --all"],
       currency: true
     });
   }
@@ -20,28 +21,32 @@ module.exports = class Sell extends Command {
       return msg.client.errors.custom(
         msg,
         msg.channel,
-        "You currently do not won any items!"
+        "You currently do not own any items!"
       );
 
-    const item = data.inv.find(
-      (i) => i.name.toLowerCase().replace(/ +/gi, "") === args[0]
+    const items = data.inv.filter(
+      (i) => i.name.toLowerCase().replace(/ +/gi, "") === args[0].toLowerCase()
     );
-    if (!item)
+    if (!items || !items[0])
       return msg.client.errors.custom(
         msg,
         msg.channel,
         "You do not own that item or it is not a valid item ID!"
       );
 
-    data.inv.splice(data.inv.indexOf(item), 1);
+    if (flags.all) for(const item of items) data.inv.splice(data.inv.indexOf(item), 1);
+    else data.inv.splice(data.inv.indexOf(items[0]), 1);
+
+    const price = flags.all ? items.reduce((p,c) => p += c.price, 0) : items[0].price;
+
     await data.save();
-    await msg.author.currency.add(item.price);
+    await msg.author.currency.add(price);
 
     msg.channel.send(
       msg.success(
-        `You sold one **${item.name}** for ${
+        `You sold ${flags.all ? items.length : "one"} **${args[0].toLowerCase()}** for ${
           msg.client.emoji.coin
-        }${item.price.toLocaleString()}`
+        }${price.toLocaleString()}`
       )
     );
   }
