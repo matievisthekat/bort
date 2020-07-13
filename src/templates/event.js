45b823663fd09a13d9b7507021151774d8dae067
MatievisTheKat <matievisthekat@gmail.com>
2020-07-13 12:07:03 +0200

    setup lib files

--- src/templates/event.js
+++
@@ -1,9 +0,0 @@
-const Event = require("../../structures/base/Event");
-
-module.exports = class extends Event {
-  constructor() {
-    super("name", "type");
-  }
-
-  async run(client, ...args) {}
-};
