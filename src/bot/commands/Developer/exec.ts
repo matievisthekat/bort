042e718a1b65ac3c726d203f2d8819eb77ea5061
MatievisTheKat <matievisthekat@gmail.com>
2020-07-14 20:26:11 +0200

    add execute command

---
+++ src/bot/commands/Developer/exec.ts
@@ -0,0 +1,39 @@
+import { Command, Arg, Bort } from "../../../lib";
+import { Message } from "discord.js";
+import { util } from "../../../lib";
+
+export default class extends Command {
+  constructor(client: Bort) {
+    super(client, {
+      name: "exec",
+      aliases: ["execute"],
+      description: "Execute a terminal command",
+      examples: ["node -v"],
+      args: [new Arg("command", true)],
+      devOnly: true,
+      __filename
+    });
+  }
+
+  public async run(msg: Message, [command, args, flags]) {
+    const options = {
+      split: {
+        char: "\n",
+        prepend: "```\n",
+        append: "```"
+      }
+    };
+
+    const res = await util.execute(args.join(" "));
+
+    for (const output of Object.entries(res)) {
+      if (output[1])
+        await msg.channel.send(
+          `**${output[0]}**\n\`\`\`\n${output[1]}\`\`\``,
+          options
+        );
+    }
+
+    await msg.success("Command successfully run");
+  }
+}
