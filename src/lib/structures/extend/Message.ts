14010aab00c81199cf455cb86ddba128e2a9a922
MatievisTheKat <matievisthekat@gmail.com>
2020-07-16 13:29:33 +0200

    clean up Message.ts

--- src/lib/structures/extend/Message.ts
+++ src/lib/structures/extend/Message.ts
@@ -1,50 +1,60 @@
 import { Bot } from "../Client";
-import { TextChannel, DMChannel, NewsChannel, MessageEmbed, Structures, Client } from "discord.js";
-import { PermissionString } from "discord.js";
+import { TextChannel, DMChannel, NewsChannel, MessageEmbed, Structures, Client, PermissionString, Message } from "discord.js";
 
-Structures.extend(
-  "Message",
-  (Message) =>
-    class extends Message {
-      constructor(client: Bot | Client, data: object, channel: TextChannel | DMChannel) {
-        super(client, data, channel);
-      }
+class CustomMessage extends Message {
+  constructor(client: Bot | Client, data: object, channel: TextChannel | DMChannel) {
+    super(client, data, channel);
+  }
 
-      /**
-       * warn
-       * @param {String|MessageEmbed} msg The message to send
-       * @returns A promise
-       * @public
-       */
-      public async warn(msg: string | MessageEmbed, channel: TextChannel | DMChannel | NewsChannel = this.channel): Promise<any> {
-        return await channel.send(`:warning: ${msg}`);
-      }
+  /**
+   * warn
+   * @param {String|MessageEmbed} msg The message to send
+   * @returns {Promise<Message>} The message that was sent
+   * @public
+   */
+  public async warn(msg: string | MessageEmbed, channel: TextChannel | DMChannel | NewsChannel = this.channel): Promise<Message> {
+    return await channel.send(`${this.emoji("warn")} ${msg}`);
+  }
 
-      /**
-       * success
-       * @param {String|MessageEmbed} msg The message to send
-       * @returns A promise
-       * @public
-       */
-      public async success(msg: string | MessageEmbed, channel: TextChannel | DMChannel | NewsChannel = this.channel): Promise<any> {
-        return await channel.send(`:white_check_mark: ${msg}`);
-      }
+  /**
+   * success
+   * @param {String|MessageEmbed} msg The message to send
+   * @returns {Promise<Message>} The message that was sent
+   * @public
+   */
+  public async success(msg: string | MessageEmbed, channel: TextChannel | DMChannel | NewsChannel = this.channel): Promise<Message> {
+    return await channel.send(`${this.emoji("success")} ${msg}`);
+  }
 
-      /**
-       * emoji
-       */
-      public emoji(type: string): string {
-        const perms: PermissionString = "USE_EXTERNAL_EMOJIS";
-        let external: boolean = true;
+  /**
+   * error
+   * @param {String|MessageEmbed} msg The message to send
+   * @returns {Promise<Message>} The message that was sent
+   * @public
+   */
+  public async error(msg: string | MessageEmbed, channel: TextChannel | DMChannel | NewsChannel = this.channel): Promise<Message> {
+    return await channel.send(`${this.emoji("error")} ${msg}`);
+  }
 
-        if (this.guild && (!this.guild.me.hasPermission(perms) || !this.guild.me.permissionsIn(this.channel).has(perms))) {
-          external = false;
-        }
+  /**
+   * emoji
+   * @param {string} type The type of emoji to search for
+   * @returns {string} The emoji string
+   * @public
+   */
+  public emoji(type: string): string {
+    const perms: PermissionString = "USE_EXTERNAL_EMOJIS";
+    let external: boolean = true;
 
-        const emoji = this.client.config.emoji[type.toLowerCase()];
-        if (!emoji) return "";
-
-        return external ? emoji.custom || emoji.default : emoji.default;
-      }
+    if (this.guild && (!this.guild.me.hasPermission(perms) || !this.guild.me.permissionsIn(this.channel).has(perms))) {
+      external = false;
     }
-);
+
+    const emoji = this.client.config.emoji[type.toLowerCase()];
+    if (!emoji) return "";
+
+    return external ? emoji.custom || emoji.default : emoji.default;
+  }
+}
+
+Structures.extend("Message", (Message) => CustomMessage);
