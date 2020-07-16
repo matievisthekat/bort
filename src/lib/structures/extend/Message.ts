b81e2f48b641faaa264b4b99614bbd6556deae14
MatievisTheKat <matievisthekat@gmail.com>
2020-07-16 13:06:39 +0200

    add emoji method

--- src/lib/structures/extend/Message.ts
+++ src/lib/structures/extend/Message.ts
@@ -1,22 +1,12 @@
 import { Bot } from "../Client";
-import {
-  TextChannel,
-  DMChannel,
-  NewsChannel,
-  MessageEmbed,
-  Structures,
-  Client
-} from "discord.js";
+import { TextChannel, DMChannel, NewsChannel, MessageEmbed, Structures, Client } from "discord.js";
+import { PermissionString } from "discord.js";
 
 Structures.extend(
   "Message",
   (Message) =>
     class extends Message {
-      constructor(
-        client: Bot | Client,
-        data: object,
-        channel: TextChannel | DMChannel
-      ) {
+      constructor(client: Bot | Client, data: object, channel: TextChannel | DMChannel) {
         super(client, data, channel);
       }
 
@@ -26,10 +16,7 @@ Structures.extend(
        * @returns A promise
        * @public
        */
-      public async warn(
-        msg: string | MessageEmbed,
-        channel: TextChannel | DMChannel | NewsChannel = this.channel
-      ): Promise<any> {
+      public async warn(msg: string | MessageEmbed, channel: TextChannel | DMChannel | NewsChannel = this.channel): Promise<any> {
         return await channel.send(`:warning: ${msg}`);
       }
 
@@ -39,11 +26,25 @@ Structures.extend(
        * @returns A promise
        * @public
        */
-      public async success(
-        msg: string | MessageEmbed,
-        channel: TextChannel | DMChannel | NewsChannel = this.channel
-      ): Promise<any> {
+      public async success(msg: string | MessageEmbed, channel: TextChannel | DMChannel | NewsChannel = this.channel): Promise<any> {
         return await channel.send(`:white_check_mark: ${msg}`);
+      }
+
+      /**
+       * emoji
+       */
+      public emoji(type: string): string {
+        const perms: PermissionString = "USE_EXTERNAL_EMOJIS";
+        let external: boolean = true;
+
+        if (this.guild && (!this.guild.me.hasPermission(perms) || !this.guild.me.permissionsIn(this.channel).has(perms))) {
+          external = false;
+        }
+
+        const emoji = this.client.config.emoji[type.toLowerCase()];
+        if (!emoji) return "";
+
+        return external ? emoji.custom || emoji.default : emoji.default;
       }
     }
 );
