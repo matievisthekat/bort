9df2368b6fbd52dedfa15fa91a7426b6846b2e23
MatievisTheKat <matievisthekat@gmail.com>
2020-07-26 19:50:36 +0200

    fix deepscan issue

--- src/commands/General/help.ts
+++ src/commands/General/help.ts
@@ -47,7 +47,7 @@ export default class extends Command {
       const command = commands.get(args.join(" ")) ?? aliases.get(args.join(" "));
       if (!command) return await msg.warn(`I could not find a command with that name! Use \`${msg.client.prefix}${this.opts.name}\` for a full list of commands`);
 
-      const formatPerms = (perms: Array<PermissionString>) => perms.map((perm) => Util.capitalise(perm.replace(/_+/gi, " ")))?.join(", ") || "None";
+      const formatPerms = (perms: Array<PermissionString>) => perms?.map((perm) => Util.capitalise(perm.replace(/_+/gi, " "))).join(", ") || "None";
 
       const opts = command.opts;
       const embed = msg.embed()
