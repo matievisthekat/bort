778cb82ee964c34a7e0a5d508a673b94e7c143e7
Matthew Stead <matthew@pop-os.localdomain>
2020-08-18 11:43:12 +0200

    update gatsby config

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: "bort",
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `The website for bort. A fun Discord bot`,
+    author: `@matievisthekat`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
