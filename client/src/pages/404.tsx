a5f9cb6fd8882fd992e36c5848e603e5cb6bb7b1
Matthew Stead <matthew@pop-os.localdomain>
2020-08-18 12:52:09 +0200

    make 404 page look nicer

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -3,12 +3,21 @@ import React from "react";
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" description="Page not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-);
+class NotFoundPage extends React.Component {
+  constructor(props: Readonly<{}>) {
+    super(props);
+  }
+  public render() {
+    return (
+      <Layout>
+        <SEO title="404: Not found" description="Page not found" />
+        <div className="container text-center text-light">
+          <h1>Not found :(</h1>
+          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        </div>
+      </Layout>
+    );
+  }
+}
 
 export default NotFoundPage;
