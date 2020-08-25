c872f7b87812d7b2a8f6a71440008f2195559277
Matthew Stead <matthew@pop-os.localdomain>
2020-08-25 14:03:48 +0200

    add login button

--- client/src/components/NavBar/Icons.tsx
+++ client/src/components/NavBar/Icons.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
 interface Props {}
 interface State {}
@@ -11,6 +12,11 @@ export default class NavbarIcons extends React.Component<Props, State> {
   public render() {
     return (
       <ul className="navbar-nav nav-flex-icons">
+        <li className="nav-item">
+          <Button as="a" href="/login" className="btn-sm">
+            Login
+          </Button>
+        </li>
         <li className="nav-item">
           <a className="nav-link" href="https://discord.gg/5j4hTMJ" target="_blank">
             <i className="fab fa-discord"></i>
