7e9085004ee72e478bf6949186928a6887b46168
MatievisTheKat <matievisthekat@gmail.com>
2020-10-20 13:01:02 +0200

    chore(scripts): update build script

--- scripts/build.cpp
+++ scripts/build.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 using namespace std;
 
+void buildBot(int *exitCode)
+{
+  *exitCode = system("npx tsc");
+  cout << "Built to ./dist/src/index.js and ./dist/api/index.js" << endl;
+}
+
+void buildClient(int *exitCode)
+{
+  *exitCode = system("cd client && npm run build");
+  cout << "Built to ./client/public/" << endl;
+}
+
+void buildImg(int *exitCode)
+{
+  *exitCode = system("cd api/image && go build main.go");
+  cout << "Built to ./api/image/main" << endl;
+}
+
+void buildScripts(int *exitCode)
+{
+  *exitCode = system("cd scripts && g++ clean.cpp -o ../bin/clean && g++ install.cpp -o ../bin/install && g++ start.cpp -o ../bin/start");
+  cout << "Built to ./bin/{install, clean, start}" << endl;
+}
+
 int main(int argc, char *argv[])
 {
   int exitCode = 0;
@@ -9,25 +33,26 @@ int main(int argc, char *argv[])
   {
     if (*argv[1] == 'b')
     {
-      exitCode = system("npx tsc");
-      cout << "Built to ./dist/src/index.js and ./dist/api/index.js" << endl;
+      buildBot(&exitCode);
     }
     else if (*argv[1] == 'i')
     {
-      exitCode = system("cd api/image && go build -o ../../dist/api/image");
-      cout << "Built to ./dist/api/image" << endl;
+      buildImg(&exitCode);
     }
     else if (*argv[1] == 'c')
     {
-      exitCode = system("cd client && npm run build");
-      cout << "Built to ./client/public/" << endl;
+      buildClient(&exitCode);
+    }
+    else if (*argv[1] == 's')
+    {
+      buildScripts(&exitCode);
     }
     else if (*argv[1] == 'a')
     {
-      system("npx tsc");
-      system("cd api/image && go build main.go");
-      system("cd client && npm run build");
-      cout << "Built to ./dist/src/index.js, ./dist/api/index.js, ./dist/api/image, ./dist/public" << endl;
+      buildBot(&exitCode);
+      buildImg(&exitCode);
+      buildClient(&exitCode);
+      buildScripts(&exitCode);
     }
     else
     {
