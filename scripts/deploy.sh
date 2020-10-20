9d47c6036f7a13fbff30226c5e7e072341e8273a
MatievisTheKat <matievisthekat@gmail.com>
2020-10-20 12:31:15 +0200

    chore(scripts): remove outdated deploy script

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-$TOKEN=$1
-$DEPLOY_DIR=$2
-$REMOTE=$3
-
-curl -H "Authorization: $TOKEN" -d "screen=$SCREEN&workingDir=$DEPLOY_DIR&remote=$REMOTE" https://api.matievisthekat.dev/pull
