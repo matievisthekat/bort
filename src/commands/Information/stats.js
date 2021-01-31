758808ee1a470260af6e4b44467c22f7ca2b97ba
MatievisTheKat <matievisthekat@gmail.com>
2021-01-31 11:14:25 +0200

    reset

--- src/commands/Information/stats.js
+++ src/commands/Information/stats.js
@@ -1,82 +1,56 @@
-const Command = require('../../structures/base/Command')
-const { version } = require('discord.js')
-const { cpu, mem, osCmd } = require('node-os-utils')
-const { check } = require('diskusage')
-const { join } = require('path')
+const Command = require("../../structures/base/Command");
+const { version } = require("discord.js");
+const { cpu, mem, osCmd } = require("node-os-utils");
 
 module.exports = class Stats extends Command {
-  constructor () {
+  constructor() {
     super({
-      name: 'stats',
-      aliases: ['statistics'],
-      category: 'Information',
-      description: 'View statistics for the bot',
+      name: "stats",
+      aliases: ["statistics"],
+      category: "Information",
+      description: "View statistics for the bot",
       requiresArgs: false
-
-    })
+    });
   }
 
-  async run (msg, args, flags) {
-    const m = await msg.channel.send(msg.loading('Fetching statistics...'))
+  async run(msg, args, flags) {
+    const m = await msg.channel.send(msg.loading("Fetching statistics..."));
 
-    const path = process.platform === 'win32' ? 'c:' : '/'
-    const files = msg.client.util
-      .findNested(join(__dirname, '..', '..', '..'))
-      .filter((p) => !p.includes('node_modules'))
-
-    const botUptime = msg.client.util.ms(msg.client.uptime, { long: true })
+    const botUptime = msg.client.util.ms(msg.client.uptime, { long: true });
     const procUptime = msg.client.util.ms(
-      msg.client.util.ms(`${process.uptime().toString().split('.')[0]}s`),
+      msg.client.util.ms(`${process.uptime().toString().split(".")[0]}s`),
       { long: true }
-    )
-    const nodeV = process.version
-    const djsV = version
+    );
+    const nodeV = process.version;
+    const djsV = version;
     const plat = msg.client.util.toProperCase(
       `${process.platform} ${process.arch}`
-    )
-    const cpuUsagePercent = await cpu.usage()
-    const amtCPUs = cpu.count()
+    );
+    const cpuUsagePercent = await cpu.usage();
+    const amtCPUs = cpu.count();
 
-    const memory = await mem.info()
-    const user = await osCmd.whoami()
+    const memory = await mem.info();
+    const user = await osCmd.whoami();
 
-    check(path, async (err, res) => {
-      if (err) return await msg.client.errors.custom(msg, msg.channel, 'Failed to check disk. Please try agian later')
+    const embed = new msg.client.Embed()
+      .addField("Versions", `**Node JS:** ${nodeV}\n**Discord.js:** ${djsV}`)
+      .addField("Uptime", `**Bot:** ${botUptime}\n**Process:** ${procUptime}`)
+      .addField(
+        "Memory & CPU",
+        `**Total Memory:** ${Math.round(
+          memory.totalMemMb
+        )} MB\n**Used:** ${Math.round(
+          memory.totalMemMb - memory.freeMemMb
+        )} MB (${Math.round(
+          100 - memory.freeMemPercentage
+        )}%)\n**Free Memory:** ${Math.round(memory.freeMemMb)} MB (${Math.round(
+          memory.freeMemPercentage
+        )}%)\n**CPU Amount:** ${amtCPUs}\n**CPU Usage:** ${Math.round(
+          cpuUsagePercent
+        )}%`
+      )
+      .addField("Host", `**Platform:** ${plat}\n**User:** ${user}`);
 
-      const free = Math.round(res.free / 1000 / 1024 / 1000)
-      const used = Math.round((res.total - res.free) / 1000 / 1024 / 1000)
-      const tot = Math.round(res.total / 1000 / 1024 / 1000)
-
-      const embed = new msg.client.Embed()
-        .addField('Versions', `**Node JS:** ${nodeV}\n**Discord.js:** ${djsV}`)
-        .addField('Uptime', `**Bot:** ${botUptime}\n**Process:** ${procUptime}`)
-        .addField(
-          'Memory & CPU',
-          `**Total Memory:** ${Math.round(
-            memory.totalMemMb
-          )} MB\n**Used:** ${Math.round(
-            memory.totalMemMb - memory.freeMemMb
-          )} MB (${Math.round(
-            100 - memory.freeMemPercentage
-          )}%)\n**Free Memory:** ${Math.round(
-            memory.freeMemMb
-          )} MB (${Math.round(
-            memory.freeMemPercentage
-          )}%)\n**CPU Amount:** ${amtCPUs}\n**CPU Usage:** ${Math.round(
-            cpuUsagePercent
-          )}%`
-        )
-        .addField('Host', `**Platform:** ${plat}\n**User:** ${user}`)
-        .addField(
-          'Disk',
-          `**Total:** ${tot} GB\n**Used:** ${used} GB (${Math.round(
-            (used / tot) * 100
-          )}%)\n**Free:** ${free} GB (${Math.round(
-            (free / tot) * 100
-          )}%)\n**Total JS Files:** ${files.length}`
-        )
-
-      m.edit('', embed)
-    })
+    m.edit("", embed);
   }
-}
+};
--- src/commands/Utility/translate.js
+++ src/commands/Utility/translate.js
@@ -1,24 +1,29 @@
-const Command = require('../../structures/base/Command')
+const Command = require("../../structures/base/Command");
 
 module.exports = class Translate extends Command {
-  constructor () {
+  constructor() {
     super({
-      name: 'translate',
-      category: 'Utility',
-      description: 'Translate between languages',
-      usage: '{to} {text}',
-      examples: ['fr Hello there', 'en Bonjour'],
-      cooldown: '15s'
-
-    })
+      name: "translate",
+      category: "Utility",
+      description: "Translate between languages",
+      usage: "{to} {text}",
+      examples: ["fr Hello there", "en Bonjour"],
+      cooldown: "15s"
+    });
   }
 
-  async run (msg, args, flags) {
+  async run(msg, args, flags) {
+    if (!msg.client.translator)
+      return await msg.channel.send(
+        msg.warning(
+          "The translator has not been initialized. This is likely a result of reaching the monthly translate limit"
+        )
+      );
     const to = msg.client.translator.langs.find(
       (lang) =>
         lang.name.toLowerCase() === args[0].toLowerCase() ||
         lang.code.toLowerCase() === args[0].toLowerCase()
-    )
+    );
     if (!to) {
       return msg.channel.send(
         msg.warning(
@@ -26,23 +31,23 @@ module.exports = class Translate extends Command {
             false
           )}languages\` to view available languages!`
         )
-      )
+      );
     }
 
     const m = await msg.channel.send(
-      msg.loading('Sending text to the server...')
-    )
+      msg.loading("Sending text to the server...")
+    );
 
     const translated = await msg.client.translator.translate(
-      args.slice(1).join(' '),
+      args.slice(1).join(" "),
       to.code
-    )
+    );
 
     m.edit(
-      msg.success(translated.text.join(' ')) ||
+      msg.success(translated.text.join(" ")) ||
         msg.warning(
           "Oops! Looks like the server didn't send a valid response! Try again later"
         )
-    )
+    );
   }
-}
+};
